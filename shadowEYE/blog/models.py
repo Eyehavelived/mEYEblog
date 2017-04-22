05ac2d21344756a9c06448f4654ed8a9c9453e15
JJ <jingjie@floatingcube.com>
2017-04-22 13:41:26 +0700

    blog app included; design files added

--- shadowEYE/blog/models.py
+++ shadowEYE/blog/models.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Blub(models.Model):
+    title = models.CharField(max_length=52)
+    slug = models.SlugField()
+    body = models.TextField()
+    subtitle = models.CharField(max_length=500)
+    create_date = models.DateTimeField('date inflated')
+    update_date = models.DateTimeField('date edited')
+
+
+
 # Create your models here.
--- shadowEYE/blog/views.py
+++ shadowEYE/blog/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'blog/index.html', {})
+
 
 # Create your views here.
