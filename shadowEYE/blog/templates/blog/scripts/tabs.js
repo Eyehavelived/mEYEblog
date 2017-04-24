937f680c5175dc5d0714185f518a8825ed31450f
JJ <jingjie@floatingcube.com>
2017-04-24 18:21:44 +0700

    tabs are finally doing the bare minimum

--- shadowEYE/blog/templates/blog/scripts/tabs.js
+++ shadowEYE/blog/templates/blog/scripts/tabs.js
@@ -1,7 +1,7 @@
 function openTab(event, tabName) {
 	var i, tabcontent, tablinks;
 
-	tabcontent = document.getElementsByClassName("tab-content");
+	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i =0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
@@ -10,6 +10,6 @@ function openTab(event, tabName) {
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
-	document.getElementsById(tabName).style.display = "block";
+	document.getElementById(tabName).style.display = "block";
 	event.currentTarget.className += " active";
 }
